a8e78efd5c0a3dd5a38ca21e531983f8a33e8ec5
10c13adityakumarsahu <10c13adityakumarsahu@gmail.com>
2026-02-01 03:34:19 +0530

    500 issue thread

--- frontend/src/pages/admin/ProjectDashboard.jsx
+++ frontend/src/pages/admin/ProjectDashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams, useNavigate, useOutletContext, useSearchParams } from "react-router-dom";
 import api from "../../api/axios";
 
