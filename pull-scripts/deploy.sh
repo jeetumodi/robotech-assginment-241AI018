5dc7c25278586447677302c471d6acd480cfb710
jeetu <jeetumodi9400@gmail.com>
2026-02-22 10:41:57 +0530

    update deploy.sh

--- pull-scripts/deploy.sh
+++ pull-scripts/deploy.sh
@@ -6,10 +6,10 @@ COMMIT_SHA=$1
 DOCKER_REPO=docker.io/jeetumodi/robotech
 
 echo "Pulling backend production image..."
-docker pull $DOCKER_REPO/backend:$COMMIT_SHA
+docker pull $DOCKER_REPO-backend:$COMMIT_SHA
 
 echo "Pulling frontend production image..."
-docker pull $DOCKER_REPO/frontend:$COMMIT_SHA
+docker pull $DOCKER_REPO-frontend:$COMMIT_SHA
 
 echo "Deploying containers using docker-compose.prod.yml..."
 docker-compose -f docker-compose.prod.yml up -d --no-build
